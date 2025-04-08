60c851b34047be6da57cd259536b3e2543d70efb
MatthewOfomi <ofomimatthew7@gmail.com>
2025-04-08 14:54:41 +0100

    feat: endpoints implemented

---
+++ insurance_api/serializers.py
@@ -0,0 +1,21 @@
+# insurance/serializers.py
+
+from rest_framework import serializers
+from .models import InsurancePolicy, ClaimsSubmission
+
+
+class ClaimsSubmissionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ClaimsSubmission
+        fields = '__all__'
+        depth = 1
+        
+    
+
+
+class InsurancePolicySerializer(serializers.ModelSerializer):
+    claims = ClaimsSubmissionSerializer(many=True, read_only=True)  # nested claims
+
+    class Meta:
+        model = InsurancePolicy
+        fields = '__all__'
